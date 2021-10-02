4d2c3ca6888ca77d463bd03a9ee84e02b0a4dc63
So Kato <hmthmtgrnl@gmail.com>
2021-10-03 01:20:28 +0900

    secret_key keep secret in github repository

--- README.md
+++ README.md
@@ -18,6 +18,8 @@ First you should install requirement, and below command execute.
 - #### #python3 -m venv myvenv
 - #### #source myvenv/bin/activate
 - #### #pip install -r requirement.txt
+- #### #git clone git@github.com:sohsoh125/bitcoinapp4.git
+- #### #python generate_secretkey_setting.py > local_settings.py
 ## Install
 - django==3.2
 ## Contribution
--- myapp/config/settings.py
+++ myapp/config/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,8 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ik)3dq!xv)-a*5v6_h8)-ui21$%f=ubcn)0n-9^^41$8-c4+*0'
+try:
+    from .local_settings import *
+except ImportError:
+    pass
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -123,3 +126,10 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# SECURITY WARNING: keep the secret key used in production secret!
+if not DEBUG:
+    # if not DEBUG, environment variable set
+    SECRET_KEY = os.environ['SECRET_KEY']
+else:
+    SECRET_KEY = SECRET_KEY
